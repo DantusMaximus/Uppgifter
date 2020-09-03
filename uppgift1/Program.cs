09cb9d944edcc49811afaad12a90f31ded612969
DantusMaximus <dante.krang@outlook.com>
2020-09-03 16:04:44 +0200

    first commit

---
+++ uppgift1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är ditt namn?");
+            string namn = Console.ReadLine();
+            Console.WriteLine("Hej " + namn + "!");
+        } 
+    }
+}
