d77694e9251950dfc55d978f65dbdbf46d919150
DantusMaximus <dante.krang@outlook.com>
2020-09-11 14:38:18 +0200

    Lekion6.1

---
+++ Lektion3/bilar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace bilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många personer?");
+            int personCount = Int32.Parse(Console.ReadLine());
+            const int carCapacity = 5;
+            int carCount = (personCount + (carCapacity-1))/carCapacity;
+            Console.WriteLine($"Det krävs {carCount} bilar och blir {carCount*carCapacity-personCount} platser över");
+        }
+    }
+}
---
+++ Lektion6/Histogram/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Histogram
+{
+    static class Histogram{
+        public static void ReadHistogram(string[] args, int[] histogram)
+        {
+            for(int i = 0; i<args.Length; i++)
+            {
+                for(int value = 1; value<=9; value++)
+                {
+                    if(args[i] == ($"{value}"))
+                    {
+                        histogram[value-1]++;
+                    }
+                }
+            }
+        }
+        public static void GraphHistogram(int[] histogram)
+        {
+            int maxLength = 0;
+            for(int i = 0; i<=8; i++)
+            {
+                if(histogram[i] > maxLength)
+                {
+                    maxLength = histogram[i];
+                }
+            }
+            for(int i = maxLength; i>0; i--)
+            {
+                for(int j = 0; j<=8; j++)
+                {
+                    if(histogram[j] >= i)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("1 2 3 4 5 6 7 8 9");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] histogram = new int[9];
+            Histogram.ReadHistogram(args, histogram);
+            Histogram.GraphHistogram(histogram);
+        }
+    }
+}
